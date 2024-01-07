58472f305fc459b5414c42860db16b489b4330a0
ebadwebdev9231 <kebad9231@gmail.com>
2024-01-07 15:19:32 +0500

    dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-import theme from "./theme.ts";
 import App from "./App.tsx";
+import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
